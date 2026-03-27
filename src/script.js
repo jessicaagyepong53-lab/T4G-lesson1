e9af8c0fdae2e26392bb4b5c149646ece5cea249
Agyepong Jessica <jessicaagyepong53@gmail.com>
2026-03-27 12:36:46 +0000

    Additions

---
+++ src/script.js
@@ -0,0 +1,34 @@
+// altering the user
+// alert("Hello User");
+let x = 2;
+
+let age; // Declaring a varible
+age = 8; // Initializing a avrible
+let name = "Jessica" // Declaring and intializing a varible
+// alert(age);
+// alert("Hello"name);
+
+// age = 10; // reassigning the vaue of age from 8 to 10
+// alert(age); // Expecting to alert 10
+
+// const age1 = 15;
+// alert(age1);
+
+// age1 = 21
+// alert(age1)
+
+// prompt("What is your name?");
+let username = prompt("What is your name?");
+alert("Welcome ❤️ " + username + "🤍🩶💜💞" + "We are excited to see you..." ); // Concatenation in activation
+
+alert(`Hello ${username}. Exicted to see you... You are ${age} years old`); // Interpolation
+// let sentence = "I " + "am " + "here.";
+alert(sentence);
+
+// let app-users = {
+//     user1: {
+//         name: "Jessica",
+//         age: 20
+//     }
+//     user2:
+// }
